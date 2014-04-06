6ba21bb0dd9f8dcff69e40d63481bfa35d29ac28
arajparaj <arajparaj@gmail.com>
2014-04-06 23:37:42 +0530

    added css

--- index.php
+++ index.php
@@ -1,24 +1,31 @@
 <?php session_start();
 if($_SESSION["login"]==1){
-	
 	header("location:home.php");
 }
 ?>
+<!DOCTYPE html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+  <section class="container">
+    <div class="login">
+      <h1>Login to PicUp</h1>
+      <form method="post" action="verify.php">
+        <p><input type="text" name="username" placeholder="Username"></p>
+        <p>
+          <input type="password" name="password" placeholder="Password">
+        </p>
+        <p class="submit">
+          <input type="submit" name="commit" value="Login">
+        </p>
+      </form>
+    </div>
 
-
-<div align="middle">
-	<h1>Welcome to PicUp!!!</h1>
-
-<form action="verify.php" method="post">
-    User Name:<br>
-    <input type="text" name="username"><br><br>
-    Password:<br>
-    <input type="password" name="password"><br><br>
-    <input type="submit" name="submit" value="Login">
-</form>
-	
-	<p>
-		Not a user? <a href="signup.php"> Sign up now </a>
-	</p>
-
-</div>
+    <div class="signup">
+      <p><h2>Not a user ??<a href="signup.php">Click Here To Sign Up</a>.</h2></p>
+    </div>
+  </section>
+</body>
+</html>
